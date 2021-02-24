464f6bc1afdf4a8c5be63a58086219153510e280
james <monaco.james18@gmail.com>
2021-02-23 21:47:59 -0600

    changed logged in user navbar, took off profile buttin and changed to logout button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal';
+// import LoginFormModal from '../LoginFormModal';
+import * as sessionActions from '../../store/session';
+import { useDispatch } from 'react-redux';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      <>
+        {/* <ProfileButton user={sessionUser} /> */}
+        <NavLink to="/form">Add Artist</NavLink>
+        <button onClick={logout}>Log Out</button>
+      </>
     );
   } else {
     sessionLinks = (
