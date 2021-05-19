2685898fc31a150c92beba212493b7426edea0a1
james <monaco.james18@gmail.com>
2021-05-18 23:21:13 -0500

    checking for fatal error

--- backend/routes/api/home.js
+++ backend/routes/api/home.js
@@ -12,6 +12,7 @@ router.get("/", async(req, res) => {
     return res.json(causes); 
 });
 //need to rethink and see if I can use a switch case 
+
 router.get("/:socialIssue", async(req, res) => {
     const issue = req.params.socialIssue;
     let newAuthToken = await getAuthToken();
